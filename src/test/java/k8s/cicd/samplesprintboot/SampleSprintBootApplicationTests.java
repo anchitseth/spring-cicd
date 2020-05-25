d37483531f361ce5e7311c16e990c10e7c9a0392
Anchit Seth <anchitseth@Anchits-MacBook-Pro.local>
2020-05-25 16:35:36 +0530

    Initial commit

---
+++ src/test/java/k8s/cicd/samplesprintboot/SampleSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package k8s.cicd.samplesprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
